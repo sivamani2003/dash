f90d5d546f0c6178961b5475ca7970e3391c5bd7
siva <sivamani_k@srmap.edu.in>
2024-07-20 12:04:05 +0530

    dash board for bob hackthon

---
+++ material-tailwind-dashboard-react-main/src/routes.jsx
@@ -0,0 +1,39 @@
+import {
+  HomeIcon,
+  UserCircleIcon,
+  TableCellsIcon,
+  InformationCircleIcon,
+  ServerStackIcon,
+  RectangleStackIcon,
+} from "@heroicons/react/24/solid";
+import { Home, Profile,} from "@/pages/dashboard";
+
+
+const icon = {
+  className: "w-5 h-5 text-inherit",
+};
+
+export const routes = [
+  {
+    layout: "dashboard",
+    pages: [
+      {
+        icon: <HomeIcon {...icon} />,
+        name: "dashboard",
+        path: "/home",
+        element: <Home />,
+      },
+      {
+        icon: <UserCircleIcon {...icon} />,
+        name: "profile",
+        path: "/profile",
+        element: <Profile />,
+      },
+      
+      
+    ],
+  },
+  
+];
+
+export default routes;
